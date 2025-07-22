3acfb04b32d494d1bd249f36b2a0f63d07441fa0
rafaelbechara <rbechara@protonmail.com>
2025-07-21 22:10:49 -0300

    feat: Atualização com settaem do bot

--- main.py
+++ main.py
@@ -1,24 +1,62 @@
 import re
+import os
 import requests
 from lxml import etree
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-user_request = input('What are you looking for?\n')
-formatted_request = user_request.title().replace(' ', '_')
-url = 'https://en.wikipedia.org/wiki/' + formatted_request
+load_dotenv()
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
-try:
-    response = requests.get(url, timeout=5)
-    response.raise_for_status() 
-except requests.exceptions.RequestException as e:
-    print(f"Error: Could not retrieve the page. {e}")
-    exit()
+def wikiscrap(user_message: str) -> str:
 
-tree = etree.HTML(response.content)
+    formatted_request = user_message.title().replace(' ', '_')
+    url = 'https://en.wikipedia.org/wiki/' + formatted_request
 
-first_paragraph = tree.xpath('string(/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[2])')
-second_paragraph = tree.xpath('string(/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[3])')
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status() 
+    except requests.exceptions.RequestException as e:
+        return f"Sorry, I couldn't retrieve the Wikipedia page for '{user_message}'. Please try again later."
 
-wikiresult = (f'\n{first_paragraph}\n{second_paragraph}')
-wikiresult = re.sub(r'\[.*?\]', '', wikiresult)
 
-print(wikiresult)
+    tree = etree.HTML(response.content)
+
+    first_paragraph = tree.xpath('string(/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[2])')
+    second_paragraph = tree.xpath('string(/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[3])')
+
+    wikiresult = (f'\n{first_paragraph}\n{second_paragraph}')
+    wikiresult = re.sub(r'\[.*?\]', '', wikiresult)
+
+    return wikiresult
+
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    user_name = update.message.from_user.first_name
+    welcome_message = (
+        f"Hi {user_name}! I'm your friendly Wikipedia Bot.\n\nJust send me a topic you want to know about, and I'll fetch a summary for you."
+    )
+    await update.message.reply_text(welcome_message)
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    user_input = update.message.text
+    summary = wikiscrap(user_input)
+    await update.message.reply_text(summary)
+
+def main() -> None:
+
+    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start_command))
+    
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
